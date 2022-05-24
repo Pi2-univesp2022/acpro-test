62b57dcb9efb33504261a0b93a7d1479660cc564
fernandonitatori <fernando.nitatori@unesp.br>
2022-05-24 20:23:13 -0300

    Security updates

--- acpro/settings.py
+++ acpro/settings.py
@@ -88,13 +88,13 @@ WSGI_APPLICATION = 'acpro.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-"""
+
 #Para usar no Heroku
 DATABASES = {
     'default': dj_database_url.config()
 
 }
-"""
+
 """
 
 DATABASES = {
@@ -109,7 +109,7 @@ DATABASES = {
 }
 """
 
-
+"""
 #Postgres
 DATABASES = {
     'default': {
@@ -121,7 +121,7 @@ DATABASES = {
         'PORT:': '5432',
     }
 }
-
+"""
 
 
 # Password validation
